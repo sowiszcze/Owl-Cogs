74ec0903bc76a24bf88233ca02c3ccfec97fdeca
Piotr Sowa <mail@piotrsowa.pl>
2020-08-18 23:13:39 +0200

    EUD compliance and minor improvements

--- urlshortener/__init__.py
+++ urlshortener/__init__.py
@@ -1,5 +1,7 @@
 from .urlshortener import UrlShortener
 
+__red_end_user_data_statement__ = "This cog does not store any user data by itself. Users wanting to remove links from Firebase should contact Support via <https://firebase.google.com/support>."
+
 def setup(bot):
     cog = UrlShortener(bot)
     bot.add_cog(cog)
--- urlshortener/urlshortener.py
+++ urlshortener/urlshortener.py
@@ -1,11 +1,9 @@
 import discord
-import logging
 import requests
 import validators
 from redbot.core import commands, checks
 from redbot.core.config import Config
-
-log = logging.getLogger("red.urlshortener")
+from typing import Literal
 
 class UrlShortener(commands.Cog):
     """
@@ -28,8 +26,7 @@ class UrlShortener(commands.Cog):
     @checks.admin_or_permissions()
     async def shortenurlset(self, ctx: commands.Context):
         """Configure UrlShortener."""
-        if ctx.invoked_subcommand:
-            return
+        pass
 
     @shortenurlset.command(name="prefix")
     async def set_prefix(self, ctx: commands.Context, prefix: str):
@@ -48,6 +45,13 @@ class UrlShortener(commands.Cog):
         - `SHORT` to generate path strings that are only as long as needed to be unique, with a minimum length of 4 characters. Use this method if sensitive information would not be exposed if a short Dynamic Link URL were guessed.
         - `UNGUESSABLE` to shorten the path to an unguessable string. Such strings are created by base62-encoding randomly generated 96-bit numbers, and consist of 17 alphanumeric characters. Use unguessable strings to prevent your Dynamic Links from being crawled, which can potentially expose sensitive information."""
 
+        if not validators.url(url):
+            return await ctx.send("You haven't provided a valid URL.")
+
+        option = option.upper()
+        if option != "UNGUESSABLE" and option != "SHORT":
+            return await ctx.send("`option` should be either `SHORT` or `UNGUESSABLE`.")
+
         firebase_keys = await self.bot.get_shared_api_tokens("firebase")
         if firebase_keys.get("api_key") is None:
             return await ctx.send("The Firebase API key has not been set. Set it with `>set api firebase api_key,<your_api_key>`.")
@@ -56,13 +60,6 @@ class UrlShortener(commands.Cog):
         if prefix is None:
             return await ctx.send("Domain URI prefix is not set. Set it with `>shortenurlset prefix <your_prefix>`.")
         
-        if not validators.url(url):
-            return await ctx.send("You haven't provided a valid URL.")
-
-        option = option.upper()
-        if option != "UNGUESSABLE" and option != "SHORT":
-            return await ctx.send("`option` should be either `SHORT` or `UNGUESSABLE`.")
-
         endpoint = "https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=" + firebase_keys.get("api_key")
         payload = { "dynamicLinkInfo": { "domainUriPrefix": prefix, "link": url }, "suffix": { "option": option }}
         
@@ -82,7 +79,7 @@ class UrlShortener(commands.Cog):
             description=("Short link: [{0}]({0})\nPreview link: [{1}]({1})".format(info["shortLink"], info["previewLink"])),
             color=await ctx.embed_color()
         )
-        if not info["warning"] is None:
+        if ctx.author.id == ctx.guild.owner_id and not info["warning"] is None:
             for warning in info["warning"]:
                 embed.add_field(
                     name="\N{WARNING SIGN} {}".format(warning["warningCode"]),
@@ -90,3 +87,14 @@ class UrlShortener(commands.Cog):
                     inline=False
                 )
         return await ctx.send(embed=embed)
+
+    async def red_delete_data_for_user(
+        self,
+        *,
+        requester: Literal["discord_deleted_user", "owner", "user", "user_strict"],
+        user_id: int,
+    ):
+        """
+            Method for finding users data inside the cog and deleting it.
+        """
+        pass
